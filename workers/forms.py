e89de017b97fc8ebd93f5d5657f32e3c85bd08d9
LouBest <wizbest@icloud.com>
2024-05-30 22:38:58 +0930

    20240530 Lastand Failed attempt at getting views to work

--- workers/forms.py
+++ workers/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms.widgets import SelectDateWidget
 from .models import Worker
 
 class WorkerForm(forms.ModelForm):
+    date_started = forms.DateField(widget=SelectDateWidget())
     class Meta:
         model = Worker
         fields = ['name', 'date_started', 'category', 'rate']
